93f5d49685c562da607c80011bac508daaf4c67f
Poi <o.onepiece9@gmail.com>
2021-12-10 22:27:47 +0700

    [ADD] Add Route Edit Product

--- start/routes.ts
+++ start/routes.ts
@@ -39,6 +39,8 @@ Route.group(() => {
   Route.get("/store/:storeID/addproduct", "ProductsController.create").as("product.add")
   Route.post("/store/:storeID/addproduct", "ProductsController.store").as("product.store")
   Route.get("/store/:storeID/deleteproduct/:productID", "ProductsController.destroy").as("product.delete")
+  Route.get("/store/:storeID/product/:productID", "ProductsController.show").as("product.show")
+  Route.post("/store/:storeID/product/:productID", "ProductsController.edit").as("product.edit")
 
 }).middleware("auth");
 
