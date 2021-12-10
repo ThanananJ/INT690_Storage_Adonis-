a25c9f606aeed9d25776776ed994982d9a124fc5
Poi <o.onepiece9@gmail.com>
2021-12-10 18:43:56 +0700

    [EDIT] Edit and Show Store

--- app/Controllers/Http/StoresController.ts
+++ app/Controllers/Http/StoresController.ts
@@ -6,12 +6,20 @@ import AddStoreValidator from "App/Validators/AddStoreValidator";
 export default class StoresController {
   public async index({ auth, view }: HttpContextContract) {
     const stores = await Store.query().where(
-      "employeeid",
+      "employeeEmployeeId",
       auth.user!.employeeId
-    );
+    ).withCount('products', (query) => {
+      query.as('productsCount')
+    });
+
+    const employee = await Employee.query().where("employee_id",
+      auth.user!.employeeId
+    ).withCount('stores', (query) => {
+      query.as('storesCount')
+    }).firstOrFail();
     // const store = await Store.all()
-    // console.log(stores)
-    return view.render("index", { stores: stores });
+    console.log(employee)
+    return view.render("index", { stores: stores, employee: employee });
   }
 
   public async create({ view }: HttpContextContract) {
@@ -30,18 +38,50 @@ export default class StoresController {
     store.ownerName = payload.ownerName
     store.ownerLastName = payload.ownerLastName
     store.telNo = payload.telNo
-    store.productCount = payload.productCount
     store.address = payload.address
-    store.employeeid = auth.user!.employeeId
+    store.employeeEmployeeId = auth.user!.employeeId
 
     await store.save()
 
     response.redirect().toRoute("index")
   }
 
-  public async show({ }: HttpContextContract) { }
+  public async show({ auth, params, view }: HttpContextContract) {
+    const storeID = params.storeID
 
-  public async edit({ }: HttpContextContract) { }
+    const store = await Store.query().where("employeeEmployeeId",
+      auth.user!.employeeId).where('storeId', storeID).withCount('products', (query) => {
+        query.as('productsCount')
+      }).firstOrFail()
+
+    console.log(store)
+
+    return view.render("storeDetail", { store: store });
+  }
+
+  public async edit({ response, auth, request, session, params }: HttpContextContract) {
+
+    const payload = await request.validate(AddStoreValidator);
+
+    const storeID = params.storeID
+
+    const store = await Store.query().where("employeeEmployeeId",
+      auth.user!.employeeId).where('storeId', storeID).firstOrFail()
+
+    store!.storeName = payload.storeName
+    store!.ownerName = payload.ownerName
+    store!.ownerLastName = payload.ownerLastName
+    store!.telNo = payload.telNo
+    store!.address = payload.address
+
+    await store?.save()
+
+    session.flash(
+      "message",
+      "Update Profile successfuly."
+    );
+    response.redirect().toRoute('store.show', { storeID: storeID })
+  }
 
   public async update({ }: HttpContextContract) { }
 
@@ -49,7 +89,7 @@ export default class StoresController {
 
     const storeID = params.storeID
 
-    const store = await Store.query().where("employeeid",
+    const store = await Store.query().where("employeeEmployeeId",
       auth.user!.employeeId).where('storeId', storeID).firstOrFail()
 
     await store?.delete()
